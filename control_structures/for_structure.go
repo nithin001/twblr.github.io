ba2b1aa2ba0b8b1f7f4e697b9913b0f8c579d595
Nithin Gunasekaran <nithin.ktg@gmail.com>
2016-07-22 16:13:14 +0530

    third exercise

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,17 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	var count int=0;
+	for{
+		if(n==1){
+			break
+		} 
+		if(n%2==0){ 
+			n/=2
+			}else{
+				n=(n*3)+1
+			}
+		count++
+	}
+	return count
 }
--- control_structures/for_structure_test.go
+++ control_structures/for_structure_test.go
@@ -3,9 +3,9 @@ package control_structures
 import "testing"
 
 func TestCollatzChainLength(t *testing.T) {
-	n := collatzChainLength(27)
+	n := collatzChainLength(67)
 
-	if n != 111 {
-		t.Errorf("Expected %d, got %d", 111, n)
+	if n != 27 {
+		t.Errorf("Expected %d, got %d", 27, n)
 	}
 }
