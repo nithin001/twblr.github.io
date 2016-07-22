75892f76e680748d85d4403bcf0d7e45f4977eb9
Nithin Gunasekaran <nithin.ktg@gmail.com>
2016-07-22 22:59:34 +0530

    map exercise

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,22 @@
 package data_structures
+import "fmt"
 
 func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+	var result []string;
+	for k, v := range data {
+        if contains(v, interest) {
+			result = append(result,k)
+		}
+    }
+    fmt.Printf("%v",result)
+    return result
 }
 
 func contains(src []string, elem string) bool {
+	for _,element := range src {
+  		if element==elem{
+  			return true
+  		}
+	}
 	return false
 }
